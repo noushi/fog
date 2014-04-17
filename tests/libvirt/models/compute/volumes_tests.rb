2cf2099c4a40ec54b24a243a4f9c11fb51ef7540
Reda NOUSHI <reda_noushi@yahoo.com>
2014-04-17 02:52:09 +0100

    . [libvirt] added a non working testcase that should pass (vol-delete)

--- tests/libvirt/models/compute/volumes_tests.rb
+++ tests/libvirt/models/compute/volumes_tests.rb
@@ -9,6 +9,24 @@ Shindo.tests('Fog::Compute[:libvirt] | volumes collection', ['libvirt']) do
     tests('should be able to get a model') do
       tests('by instance uuid').succeeds { volumes.get volumes.first.id }
     end
+    tests('should be able to delete a volume') do
+      puts "#{volumes.size}"
+      new_vol = volumes.first.clone_volume('new_vol')
+      vol_name = "fog-shindo-libvirt-#{rand(10000)}.qcow2"
+      puts vol_name
+      volumes.create(:name => vol_name,
+                     :allocation => "10M",
+                     :capacity => '10M',
+                     :format_type => 'qcow2',
+                     :pool_name => 'default')
+      initial_count = volumes.size
+      volumes.destroy new_vol.id #should maybe catch some exception here
+      volumes.reload
+      test('by id') do
+        puts "#{initial_count} == (#{volumes.size} + 1)"
+        initial_count == (volumes.size + 1) 
+      end
+    end
   end
 
 end
